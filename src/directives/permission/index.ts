3d04377efefdafcc5f65ad906134cfaec45a7835
zhangbinjie <1435255141@qq.com>
2022-11-15 18:19:56 +0800

    update

--- src/directives/permission/index.ts
+++ src/directives/permission/index.ts
@@ -3,23 +3,43 @@
  * @Author: jrucker
  * @Date: 2020-12-28 10:39:21
  * @LastEditors: jrucker
- * @LastEditTime: 2022/07/12 15:59:50
+ * @LastEditTime: 2022/11/15 18:17:14
+ * @examples:
+ * 单权限验证 v-permission="'权限名称'"
+ * 单个验证 v-permission="[flag, '权限名称']"
+ * 多权限验证 v-permission="[flag, ['权限名称']]"
  */
 
 import { useStore } from '@/views/admin/store'
 import { Directive } from 'vue'
-import { isArray } from '@/utils'
+import { isArray, isString } from '@/utils'
 
 function removeNode(el) {
   el.parentNode && el.parentNode.removeChild(el)
 }
 
-function checkPermission(el, value) {
+function checkFlag(type, el, val) {
   const perms = useStore().state.permission.accessedCodes
-  if (!isArray(value)) throw new Error(`Need perms like v-permission="['权限名称']"`)
-  if (value.length) {
-    if (!perms.some(perm => value.includes(perm))) {
-      removeNode(el)
+  if (type === 'string' && !perms.includes(val)) {
+    return removeNode(el)
+  }
+  if (type === 'boolean' && !perms.some(perm => val.includes(perm))) {
+    return removeNode(el)
+  }
+}
+
+function checkPermission(el, value) {
+  if (!value) throw new Error(`need perms like v-permission="'权限名称'"`)
+  if (isString(value)) {
+    return checkFlag('string', el, value)
+  }
+  if (isArray(value)) {
+    const [flag, val] = value
+    if (flag) return
+    if (isArray(val)) {
+      return checkFlag('boolean', el, val)
+    } else {
+      return checkFlag('string', el, val)
     }
   }
 }
